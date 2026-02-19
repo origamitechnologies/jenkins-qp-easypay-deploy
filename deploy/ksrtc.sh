bc5a5777f682e2fe54a27473c888e9cb26bacd45
origamitechnologies <origamitech007@gmail.com>
2026-02-19 13:59:45 +0530

    Create ksrtc.sh for deployment automation
    Add deployment script for KSRTC payments application.

--- deploy/ksrtc.sh
+++ deploy/ksrtc.sh
@@ -1,1 +1,15 @@
 
+#!/bin/sh
+set -e
+
+APP_DIR="/opt/ksrtc_payments/qp-easypay"
+BRANCH="main"
+
+cd "$APP_DIR"
+
+git fetch origin
+git reset --hard origin/$BRANCH
+git clean -fd
+
+php artisan optimize:clear
+php artisan optimize
