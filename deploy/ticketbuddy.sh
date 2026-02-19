62550058d41c67907a3872b596672554a748cdc0
origamitechnologies <origamitech007@gmail.com>
2026-02-19 14:01:02 +0530

    Update ticketbuddy.sh

--- deploy/ticketbuddy.sh
+++ deploy/ticketbuddy.sh
@@ -1,1 +1,14 @@
+#!/bin/sh
+set -e
 
+APP_DIR="/opt/ticketbuddy_payments/qp-easypay"
+BRANCH="main"
+
+cd "$APP_DIR"
+
+git fetch origin
+git reset --hard origin/$BRANCH
+git clean -fd
+
+php artisan optimize:clear
+php artisan optimize
